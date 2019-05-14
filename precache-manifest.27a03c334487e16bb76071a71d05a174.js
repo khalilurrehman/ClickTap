90df367927081fe5d6b0647d137c6a0f01349450
Khalilurrehman <khalil_rehman1993@ymail.com>
2019-05-14 18:24:48 +0400

    Updates

---
+++ precache-manifest.27a03c334487e16bb76071a71d05a174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d257e829e08798e3ce35699c17914906",
+    "url": "/ClickTap/index.html"
+  },
+  {
+    "revision": "79f1537628ee6948c781",
+    "url": "/ClickTap/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c26c26a79bea805ff4cf",
+    "url": "/ClickTap/static/css/main.0226289b.chunk.css"
+  },
+  {
+    "revision": "79f1537628ee6948c781",
+    "url": "/ClickTap/static/js/2.3738b18c.chunk.js"
+  },
+  {
+    "revision": "c26c26a79bea805ff4cf",
+    "url": "/ClickTap/static/js/main.e21ef2a1.chunk.js"
+  },
+  {
+    "revision": "1972b1e6518e77d219ee",
+    "url": "/ClickTap/static/js/runtime~main.cb142b74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickTap/static/media/logo.5d5d9eef.svg"
+  }
+]);
